01068876f5eddc25ec42bdd55e19a4d12102b43c
Brojowski <alex.gajowski@gmail.com>
2020-10-25 18:20:09 -0400

    exit process with error code, if any assertions failed

--- postman-tests.js
+++ postman-tests.js
@@ -4,7 +4,10 @@ const newman = require('newman')
 newman.run({
     collection: require('./KHE-API-tests.postman_collection.json'),
     reporters: 'cli',
-}, function () {
+}, function(err, summary) {
     server.close();
-})
+    if (err || summary.run.failures.length) {
+        process.exit(1);
+    }
+});
 
