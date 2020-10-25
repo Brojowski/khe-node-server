778392250066f7f79e43eded66c35ac1b7ab5eb1
Brojowski <alex.gajowski@gmail.com>
2020-10-25 18:23:33 -0400

    say hi to chris

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello Chris!'));
 
 var port = 3000;
 if (process.env.PORT) {
